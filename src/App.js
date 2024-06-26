17b44a3e537234460221f23ec9d473028923e98f
Andile18 <zama.andile18@gmail.com>
2024-06-26 02:28:00 +0200

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ export default  function App() {
   return (
     <div className="App">
      Weather App
+     <footer>
+      This project was coded by Zama Andile Dlamini & it is {" "}  
+      <a href="https://github.com/Andile18/react-weather-app" 
+      target="_blank" rel="noreferrer">
+        
+        open-sourced on Github
+        </a>
+         </footer>
     </div>
   );
 }
